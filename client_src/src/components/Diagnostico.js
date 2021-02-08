5a3f4449113899efb4afdcad8669621271b11d13
Armuto-Live <luisarmuto3@gmail.com>
2021-02-08 12:02:11 -0500

    add chatbot

--- client_src/src/components/Diagnostico.js
+++ client_src/src/components/Diagnostico.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-
+import './Diagnostico.css';
 
 
 const Diagnostico = () => (
-  <div>
-      
+  <div className="Diagnostico">
+      <iframe src="https://webchat.snatchbot.me/09c9061a53043961a3464b01a9e46e66585bb85448e57502ddc4bf7be33a5474" allow="microphone*"  height="450px" width="450px"></iframe>
   </div>
 )
 
